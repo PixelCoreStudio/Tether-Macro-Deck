03e87d68b20734ba44830f420fc3b53881704e95
PixelCoreStudio <business.pixelcore@gmail.com>
2026-07-17 17:57:08 +0200

    Clean up comments in Main.cs
    Removed comments related to plugin activation and HTTP action registration.

--- Main.cs
+++ Main.cs
@@ -6,11 +6,8 @@ namespace VoidCore.Tether
 {
     public class Main : MacroDeckPlugin
     {
-        // Dieses Event wird aufgerufen, wenn Macro Deck das Plugin aktiviert.
-        // Alle Metadaten (Name, Entwickler, Beschreibung) kommen jetzt rein aus der plugin.json!
         public override void Enable()
         {
-            // Hier registrieren wir unsere HTTP-Aktion
             this.Actions = new List<PluginAction>
             {
                 new HttpRequestAction()
